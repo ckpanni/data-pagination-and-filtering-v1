1b17e1cc2c0c75f4f562f1915cb941f045ab843f
ckpanni <catherinekpanni@gmail.com>
2024-09-29 19:25:49 -0500

    adding pagination

--- js/script.js
+++ js/script.js
@@ -18,13 +18,47 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const showPage = (list, page) => {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
 
+   const studentList = document.querySelector('.student-list');
+   const html = '';
+   studentList.innerHTML = '';
+
+   list.forEach((student, i) => {
+      if (i >= startIndex && i < endIndex ) {
+         html += `<li class="student-item cf">
+                        <div class="student-details">
+                           <img class="avatar" src="${student.picture.medium}" alt="Profile Picture">
+                           <h3>${student.name.first + student.name.last}</h3>
+                           <span class="email">${student.email}</span>
+                        </div>
+                        <div class="joined-details">
+                           <span class="date">Joined ${student.registered.date}</span>
+                        </div>
+                     </li>`;
+      }
+   })
+
+   return studentList.insertAdjacentHTML("beforeend", html)
+}
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+const addPagination = (list) => {
+  const numOfButtons = list.length / 9;
+  const linkList = document.querySelector('.link-list');
+  linkList.innerHTML = '';
 
+  for (let i = 0; i < numOfButtons; i++) {
+   ` <li>
+   <button type="button">1</button>
+ </li>`
+  }
+}
 
 
 // Call functions
