e0c13ab7c853a4069eed8ea6fc4ae9ccb5d20162
ckpanni <catherinekpanni@gmail.com>
2024-10-01 20:09:29 -0500

    redid project

--- js/script.js
+++ js/script.js
@@ -12,53 +12,46 @@ For assistance:
 */
 
 
+const studentsPerPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 const showPage = (list, page) => {
-   const startIndex = (page * 9) - 9;
-   const endIndex = page * 9;
+ const startIndex = (page * studentsPerPage) - studentsPerPage;
+ const endIndex = page * studentsPerPage;
 
-   const studentList = document.querySelector('.student-list');
-   const html = '';
-   studentList.innerHTML = '';
+ const studentList = document.querySelector('.student-list');
+ studentList.innerHTML = '';
+//  const textBlock = '';
 
-   list.forEach((student, i) => {
-      if (i >= startIndex && i < endIndex ) {
-         html += `<li class="student-item cf">
-                        <div class="student-details">
-                           <img class="avatar" src="${student.picture.medium}" alt="Profile Picture">
-                           <h3>${student.name.first + student.name.last}</h3>
-                           <span class="email">${student.email}</span>
-                        </div>
-                        <div class="joined-details">
-                           <span class="date">Joined ${student.registered.date}</span>
-                        </div>
+ for (let i = 0; i < list.length; i++) {
+
+   if (i >= startIndex && i < endIndex) {
+     const html = `<li class="student-item cf">
+                     <div class="student-details">
+                        <img class="avatar" src="${i.picture.large}" alt="Profile Picture">
+                        <h3>${i.name.first} ${i.name.last}</h3>
+                        <span class="email">${i.email}</span>
+                     </div>
+                     <div class="joined-details">
+                        <span class="date">${i.registered.date}</span>
+                     </div>
                      </li>`;
-      }
-   })
+      
+      studentList.insertAdjacentHTML("beforeend", html);
+   }
+ }
 
-   return studentList.insertAdjacentHTML("beforeend", html)
 }
+
+
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-const addPagination = (list) => {
-  const numOfButtons = list.length / 9;
-  const linkList = document.querySelector('.link-list');
-  linkList.innerHTML = '';
-
-  for (let i = 0; i < numOfButtons; i++) {
-   ` <li>
-   <button type="button">1</button>
- </li>`
-  }
-}
 
 
 // Call functions
